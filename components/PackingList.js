ec8483e1fb7123e0983dff38e4c22fd80644b11d
Diego Carvalho <108098475+DiegoCrvlho@users.noreply.github.com>
2023-12-31 18:18:23 -0300

    Update PackingList.js

--- components/PackingList.js
+++ components/PackingList.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Item from "./Item";
 
 export default function PackingList({
@@ -7,25 +6,10 @@ export default function PackingList({
   onToggleItems,
   onClearList,
 }) {
-  const [sortBy, setSortBy] = useState("input");
-
-  let sortedBy;
-
-  if (sortBy === "input") sortedBy = items;
-
-  if (sortBy === "description")
-    sortedBy = items
-      .slice()
-      .sort((a, b) => a.description.localeCompare(b.description));
-
-  if (sortBy === "packed")
-    sortedBy = items
-      .slice()
-      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul>
-        {sortedBy.map((item) => (
+        {items.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -34,12 +18,7 @@ export default function PackingList({
           />
         ))}
       </ul>
-      <div className="actions">
-        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-          <option value={"input"}>Sort By Input</option>
-          <option value={"description"}>Sort By Description</option>
-          <option value={"packed"}>Sort By Packed</option>
-        </select>
+      <div className="clearList">
         <button onClick={() => onClearList()}>Clear List</button>
       </div>
     </div>
